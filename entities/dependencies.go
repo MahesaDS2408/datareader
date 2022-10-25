e5c2544f1543278928c0e4577dddd551eaf02e70
frederett <frederettd@mail.ee>
2022-10-25 22:29:59 +0700

    feature-fix: read all csv in a row

--- entities/dependencies.go
+++ entities/dependencies.go
@@ -1,7 +1,6 @@
 package entities
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,8 +8,6 @@ import (
 	"strings"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/render"
 	"github.com/spf13/viper"
 	"golang.org/x/sync/errgroup"
 )
@@ -62,72 +59,16 @@ type HTTPResponse struct {
 	Content  interface{} `json:"content"`
 }
 
-func NewHttpRoute(d *Dependencies) *chi.Mux {
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
-
-	r.Get("/{alamat}", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "application/json")
-		if d == nil {
-			err := HTTPResponse{
-				Code:     500,
-				Messsage: "CSV Map not loaded, check your config or your csvs folder",
-				Content:  "",
-			}
-			msg, _ := json.Marshal(err)
-			w.Write(msg)
-			return
-		}
-		csvs := d.GetCSVMap()
-		if csvs == nil {
-			err := HTTPResponse{
-				Code:     500,
-				Messsage: "CSV Map not loaded, check your config or your csvs folder",
-				Content:  "",
-			}
-			msg, _ := json.Marshal(err)
-			w.Write(msg)
-			return
-		}
-		alamat := chi.URLParam(r, "alamat")
-		if csv := (*csvs)[alamat]; csv != nil {
-			queryString := r.URL.Query().Get("allowed")
-			if queryString != "" {
-				data := FilterField(csv.Data, strings.Split(queryString, ","))
-				jsonCsv, _ := json.Marshal(data)
-				w.Write(jsonCsv)
-			}
-
-			return
-		}
-
-		err := HTTPResponse{
-			Code:     404,
-			Messsage: "CSV block not found for that address",
-			Content:  "",
-		}
-		msg, _ := json.Marshal(err)
-		w.Write(msg)
-	})
-
-	return r
-}
-
 func (d Dependencies) Run() {
 	fmt.Println("Loading all csv")
 	d.SetCSVMap()
 
 	d.Router = NewHttpRoute(&d)
 
-	fmt.Println(
-		fmt.Sprintf(
-			"Datareader use %s config and online at %d",
-			viper.ConfigFileUsed(),
-			d.Config.NomorPort,
-		),
+	fmt.Printf(
+		"Datareader use %s config and online at %d \n",
+		viper.ConfigFileUsed(),
+		d.Config.NomorPort,
 	)
 
 	fmt.Println("Support me by sponsoring on GitHub at https://github.com/frederett")
